4caef4d76df00c82326148d81dc3e4457d86730b
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-07 01:19:13 +0000

    fixed the reference

--- migrations/20180207004009_create_foreignKey.js
+++ migrations/20180207004009_create_foreignKey.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.table('milestones', function (table) {
-                table.integer('famous_person_id')
+                table.integer('famous_person_id').references('id').inTable('famous_people')
         })
     ])
 };
